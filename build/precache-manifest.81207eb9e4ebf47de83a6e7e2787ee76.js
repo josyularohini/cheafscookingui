55d06c6f95cf53bef0082f56a613603f66cf0b91
josyularohini <rohini.josyula@gmail.com>
2020-09-02 22:10:47 +0530

    commit

---
+++ build/precache-manifest.81207eb9e4ebf47de83a6e7e2787ee76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a143dfa2b5536bfd8959474e13426ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa4c90a204d1e5ef747",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "499b587609d31ce35f92",
+    "url": "/static/css/main.9177edaf.chunk.css"
+  },
+  {
+    "revision": "afa4c90a204d1e5ef747",
+    "url": "/static/js/2.be9b9419.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.be9b9419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499b587609d31ce35f92",
+    "url": "/static/js/main.6c1cdbf1.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
